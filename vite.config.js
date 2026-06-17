72eec49f0b29a4f28788dad625cfef018064d94a
DIARRASSOUBA Mouhammad Lamine <diarrassoubaml440-dotcom>
2026-06-17 20:10:10 +0000

    Correction Vite pour GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WAVE-CI/',
+  base: '/Wave-ci/',
   plugins: [vue()],
   resolve: {
     alias: {
-      // "@" pointe vers le dossier src/ → on écrit @/components/... partout
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
